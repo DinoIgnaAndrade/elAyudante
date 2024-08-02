01d9c34a0bf292e0ecd07205a1e61140dc01d946
DinoIgnaAndrade <127141499+DinoIgnaAndrade@users.noreply.github.com>
2024-08-02 01:00:33 -0300

    Actualizazcion elayudante rebuild

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+import logo from '../../media/logo.jpg'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    return (
+        <nav className={styles.container}>
+
+            <div className={styles.logoContainer}>
+                <Image className={styles.logo} src={logo} alt='Logo' />
+                <Link href='/'><h1 className={styles.logoTitle}>El Ayudante</h1></Link>
+            </div>
+
+            <div className={styles.linksContainer}>
+
+                <a className={styles.link} href="#contact">Contacto</a>
+                <a className={styles.link} href="#services">Servicios</a>
+                <a className={styles.link} href="#presentation">Conocenos</a>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
